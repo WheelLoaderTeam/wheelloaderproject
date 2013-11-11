d73d57f6c3623ec780d98589eaf175c608ed91d3
Daniel <sarki024@umn.edu>
2013-11-11 16:19:19 +0100

    Daniel Updated the file Logger.c, This change now implements a early working version of the Logger we will be using.

--- LoggingCode/Logger.c
+++ LoggingCode/Logger.c
@@ -1,5 +1,5 @@
 #include "stdio.h"
-
+#include "time.h"
 //logging functions
 
 int  LogRealTime(int time, int packetID, char *event, int data1, int data2, int data3)
@@ -9,21 +9,21 @@ int  LogRealTime(int time, int packetID, char *event, int data1, int data2, int 
 
 int LogToFile(int time, int packetID, char *event, int data1, int data2, int data3)
 {
-    FILE *fp = fopen("logfile_TimeStamp.txt", "a");                                             //open the file
+    FILE *fp = fopen("LoggingTest.txt", "a");                                             //open the file
     fprintf(fp, "Log: %8d %4d %s  %d  %d  %d \n", time, packetID, event, data1, data2, data3); //print passed data to file
     fclose(fp);                                                                                //close file
 }
 
 int main()
 {
-    int faketime = 03191993;
+    int faketime = 3191993;
     int fakepacketID = 1667;
     char *fakeevent = "MidNovemberTesting";
-    int fakedata1 = 0123;
+    int fakedata1 = 123;
     int fakedata2 = 3456;
     int fakedata3 = 78910;
 
-    int testlength = 1000;
+    int testlength = 100;
     while(testlength--)
     {
      LogToFile(faketime, fakepacketID, fakeevent, fakedata1, fakedata2, fakedata3);
