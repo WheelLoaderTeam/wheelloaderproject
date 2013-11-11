c4640914f595957e5cb09d7e6907d9a155f1a63c
Daniel <sarki024@umn.edu>
2013-11-11 15:59:10 +0100

    Daniel Adding Code for the Logging portion of the software

--- Logger.c
+++ Logger.c
@@ -2,15 +2,15 @@
 
 //logging functions
 
-int  LogRealTime(int time, int packetID, int event, int data1, int data2, int data3)
+int  LogRealTime(int time, int packetID, char *event, int data1, int data2, int data3)
 {
-    printf("Log: %9d %4d %s  %d  %d  %d \n"); //format data appropriately
+    printf("Log: %8d %4d %s  %d  %d  %d \n", time, packetID, event, data1, data2, data3); //format data appropriately
 }
 
-int LogToFile(int time, int packetID, int event, int data1, int data2, int data3)
+int LogToFile(int time, int packetID, char *event, int data1, int data2, int data3)
 {
-    FILE *fp = fopen("logfile_TimeStamp.txt", ab+)                                             //open the file
-    fprintf(fp, "Log: %9d %4d %s  %d  %d  %d \n", time, packetID, event, data1, data2, data3); //print passed data to file
+    FILE *fp = fopen("logfile_TimeStamp.txt", "a");                                             //open the file
+    fprintf(fp, "Log: %8d %4d %s  %d  %d  %d \n", time, packetID, event, data1, data2, data3); //print passed data to file
     fclose(fp);                                                                                //close file
 }
 
@@ -18,7 +18,7 @@ int main()
 {
     int faketime = 03191993;
     int fakepacketID = 1667;
-    int fakeevent = "MidNovemberTesting";
+    char *fakeevent = "MidNovemberTesting";
     int fakedata1 = 0123;
     int fakedata2 = 3456;
     int fakedata3 = 78910;
