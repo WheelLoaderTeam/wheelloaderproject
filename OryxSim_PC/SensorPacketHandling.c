419f5612cda24b722c2843ed7e7282b9ddbe9a67
WLT <weel@team.com>
2013-11-15 11:39:34 +0100

    Max I thought about different received frequencies in the Sensorhandling file have to ask Andrey about that

--- OryxSim_PC/SensorPacketHandling.c
+++ OryxSim_PC/SensorPacketHandling.c
@@ -1,7 +1,7 @@
 #include <sys/time.h>
 #include <unistd.h>
 #include "Socket.h"
-#include "log"
+
 
 
 // argv[1] is the frequency parameter to send packet to simulator
@@ -16,17 +16,27 @@ int main(int argc, char *argv[]){
     struct timeval tv;
     int retval, mode;
     float buf [LEN_BUF_SENSOR];
-    /*TODO il faut remettre les valeurs du timer á chaque tour de boucle sinon ca marche pas */
+    //float* last_values[LEN_BUF_SENSOR];
     sscanf(argv[1],"%d",&mode);
     switch (mode){
         case 1 :
         {//100Hz
-            printf("mode 1\n");
+            printf("mode 1: receive 100Hz send 100Hz\n");
             break;
         }
         case 2 :
         {//50Hz
-            printf("mode 1\n");
+            printf("mode 2: receive 100Hz send 50Hz\n");
+            break;
+        }
+        case 3:
+        {//50Hz
+            printf("mode 3: receive 50Hz send 100Hz\n");
+            break;
+        }
+        case 4:
+        {//50Hz
+            printf("mode 4: receive 50Hz send 50Hz\n");
             break;
         }
         default:
@@ -35,18 +45,20 @@ int main(int argc, char *argv[]){
 
     }
 
-    if (initSocket(IMU_PORT,MOV_PORT,s_onBoard, s_sim, SIM_IP,insock,outsock)==1){
+    if (initSocket(IMU_PORT,MOV_PORT,s_onBoard, s_sim, "127.0.0.1",insock,outsock)==1){
         printf ("init success\n");
 
         while(1){
 
-           if ((recv_len = recvfrom(s_onBoard, buf, LEN_BUF_SENSOR, 0, (struct sockaddr *) &outsock, (socklen_t*) &slen)) == -1){
-                die("recvfrom()");
-            }
+           //is it a problem when the packets are received at 50Hz?
 
             if(mode == 1 ){
+                if ((recv_len = recvfrom(s_onBoard, buf, LEN_BUF_SENSOR, 0, (struct sockaddr *) &outsock, (socklen_t*) &slen)) == -1){
+                    die("recvfrom()");
+                }
                 printf("i'm in mode 1\n");
-                tv.tv_sec=1;
+                //need to reset timer at every loop
+                tv.tv_sec=1; //TODO put the real value
                 tv.tv_usec=10000;
                 retval= select(1,NULL,NULL,NULL, &tv);
                 if (retval == -1)
@@ -54,7 +66,7 @@ int main(int argc, char *argv[]){
                 else
                     printf("I have waited 10 mseconds.\n");
                     //send the message
-                    if (sendto(s_sim, buf, strlen(buf) , 0 , (struct sockaddr *) &outsock, (socklen_t*) &slen)==-1)
+                    if (sendto(s_sim, buf, (LEN_BUF_SENSOR*sizeof(float)) , 0 , (struct sockaddr *) &outsock, slen)==-1)
                     {
                         die("sendto()");
                     }
@@ -62,14 +74,64 @@ int main(int argc, char *argv[]){
             }
 
             else if(mode == 2 ){
-                printf("i'm in mode 2\n");
-                tv.tv_sec=2;
+                if ((recv_len = recvfrom(s_onBoard, buf, LEN_BUF_SENSOR, 0, (struct sockaddr *) &outsock, (socklen_t*) &slen)) == -1){
+                    die("recvfrom()");
+                }
+
+
+                //need to reset timer at every loop
+                tv.tv_sec=2;//TODO put the real value
                 tv.tv_usec=20000;
                 retval= select(1,NULL,NULL,NULL, &tv);
                 if (retval == -1)
                     perror("select()");
-                else
-                printf("I have waited 20 mseconds.\n");
+                else {
+
+                    printf("I have waited 20 mseconds.\n");
+                    if (sendto(s_sim, buf, (LEN_BUF_SENSOR*sizeof(float)) , 0 , (struct sockaddr *) &outsock, slen)==-1)
+                    {
+                        die("sendto()");
+                    }
+
+                }
+
+            }
+            else if(mode == 3 ){
+                if ((recv_len = recvfrom(s_onBoard, buf, LEN_BUF_SENSOR, 0, (struct sockaddr *) &outsock, (socklen_t*) &slen)) == -1){
+                    die("recvfrom()");
+                }
+
+
+                //need to reset timer at every loop
+                tv.tv_sec=2;//TODO put the real value
+                tv.tv_usec=10000;
+                retval= select(1,NULL,NULL,NULL, &tv);
+                if (retval == -1)
+                    perror("select()");
+                else {
+
+                    printf("I have waited 10 mseconds.\n");
+                    if (sendto(s_sim, buf, (LEN_BUF_SENSOR*sizeof(float)) , 0 , (struct sockaddr *) &outsock, slen)==-1)
+                    {
+                        die("sendto()");
+                    }
+
+                }
+                 //need to reset timer at every loop
+                tv.tv_sec=2;//TODO put the real value
+                tv.tv_usec=10000;
+                retval= select(1,NULL,NULL,NULL, &tv);
+                if (retval == -1)
+                    perror("select()");
+                else {
+
+                    printf("I have waited 10 mseconds.\n");
+                    if (sendto(s_sim, buf, (LEN_BUF_SENSOR*sizeof(float)) , 0 , (struct sockaddr *) &outsock, slen)==-1)
+                    {
+                        die("sendto()");
+                    }
+
+                }
 
             }
             else printf("my mode doesn't exist");
