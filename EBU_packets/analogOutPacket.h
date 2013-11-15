b830237eb2f1d04f34c1d540872c9da9bb29ae4b
Marcus Rdman <marrdm-9@student.ltu.se>
2013-11-15 08:25:28 +0100

    added analogInPacket

--- EBU_packets/analogOutPacket.h
+++ EBU_packets/analogOutPacket.h
@@ -4,7 +4,7 @@ typedef struct EBU_analog_out_packet{
 	uint16_t channel[24];
 } EBU_analog_out_packet;
 
-//TODO: define initial values of EBU_analog_out_packet.
+//TODO: define initial values of a new EBU_analog_out_packet.
 
 void setAnalogOut(EBU_analog_out_packet *packet, int channel, float value);
 float getAnalogOut(EBU_analog_out_packet *packet, int channel);
