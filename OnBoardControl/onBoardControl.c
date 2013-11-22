79c61ab8e85f65696620c33e3ab7d0e821f20ec6
Marcus Rådman <marrdm-9@student.ltu.se>
2013-11-22 10:09:35 +0100

    updated onboardcontrol

--- OnBoardControl/onBoardControl.c
+++ OnBoardControl/onBoardControl.c
@@ -5,38 +5,50 @@
 #define SHORT_TIMEOUT {0, 100000000}
 
 int main(void){
+	//variable definitions:
+	int bufferEmpty = 1;
+	EBUanalogOut buffer;
+	uint32_t lastPacketID;
+	struct timespec timeOflastPacketArrival;
+	
+	struct timespec timeOflastPacketSent;
+	
 	//setup sockets to send packets to the EBU
 	//send stop packet
 	//send relay packet
 	
 	//initServerSocket to receve packets from simulator
+	//setup filedescriptors for select to wait for.
 	//set long timeout
 	
 	while(1){
 		//wait for packet from simulator
 		
-		//if timeout
-			//if buffer empty
+		//if //timeout
+			if(bufferEmpty){
 				//send stop packet
 				//set long timeout
-			//else buffer not empty
+			} else{ //buffer not empty
 				//send packet in buffer
 				//clear buffer
 				//set long timeout
-		//else if incomming packet
-			//if buffer empty
+			}
+		//else //incomming packet
+			if(bufferEmpty){
 				//if (time since last packet sent > short timeout)
 					//send packet imediatly
 					//set long timeout
 				//else (time since last packet sent < short timeout)
 					//put packet in buffer
 					//set remaining time of short timeout
-			//else buffer not empty
+			} else{ //buffer not empty
 				//determine witch packet should remain in buffer
 				//set remaining time of short timeout
+			}
 	}
 	return 1;
 }
+
 
 int commandPacket2EBUpacket(CommandPacket* commandPacket, EBUanalogOut* analogEBUpacket){
 	
