e0bbf23614f43d572c60e9362d8ec201707869e7
Daniel <sarki024@umn.edu>
2013-11-29 13:44:38 +0100

    Compiles properly, need to write sendData() function before testing.

--- GyroAccCalculation/parse.c
+++ GyroAccCalculation/parse.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 #include <float.h> 				//DBL_MAX
 #include <math.h> 				//asin
-#define ss_checkfrequency 5		//ss = StationaryState
-#define ss_deltathreshold 0.50
-#define ss_numrecords	  200   //At 100 hz = 2 seconds of data
-#define end_rcv_buf    	  1024	//Adjust depending on size of buffer
-#define begin_rcv_buf     0
-//Capital Letters
+#define SS_CHECKFREQUENCY 5		//ss = StationaryState
+#define SS_DELTATHRESHOLD 0.50
+#define NUM_OF_SS_RECORDS 200   //At 100 hz = 2 seconds of data
+#define END_RCV_BUF    	  1024	//Adjust depending on size of buffer
+#define BEGIN_RCV_BUF     0
 
-//Testing Git on Personal Machine - Comment should show up in commit
+
+
 typedef struct{
 	float xAxis;
 	float yAxis;
@@ -21,6 +21,7 @@ typedef struct{
     float roll;
 }abs_pos;
 
+//This struct and function decleration are to be declared in rcvData.c, used here now for testing. 
 typedef struct{
     float posX;
     float posY;
@@ -29,7 +30,7 @@ typedef struct{
     float rotY;
     float rotZ;
 }packet_load;
-	
+packet_load recvData();
 
 
 int parseFile(char *filename);
@@ -38,7 +39,6 @@ abs_pos getAbsPos(int rcv_buf_pos);
 gyro_bias getGyroBias(int rcv_buf_pos);
 void sendData(packet_load packet_old, packet_load packet_new, gyro_bias bias);
 void sendAccData();
-//packet_load rcvData();
 
 int main(int argc, char *argv[]) {
 	int rcv_buf_pos;
@@ -51,11 +51,11 @@ int main(int argc, char *argv[]) {
 
 	while(1){
 		packet_old = packet_new;
-		//packet_new = rcvData(); //rcvPacket to be written by Jesper
+		packet_new = recvData(); //rcvPacket to be written by Jesper
 		//Create circular buffer and store packet_new here, updating buffer ptr
 		sendData(packet_old, packet_new, bias);
 		counter++;
-		if(counter == ss_numrecords){
+		if(counter == NUM_OF_SS_RECORDS){
 			counter = 0;
 			if(checkIfSS(rcv_buf_pos)){
 				radians = getAbsPos(rcv_buf_pos);
@@ -118,17 +118,17 @@ int parseFile(char *filename) {
 
 /*
 checkIfSS(int buf_pos)
-This function checks if the tractor is in a stationary state. A preset number of latest records(ss_numrecords) are read out of the accelerometer buffer. If each accelerometer axis shows a delta smaller than ss_deltathreshold , then the tractor is assumed to be stationary, and the function returns 1. If any accelerometer delta exceeds ss_deltathreshold, the function returns 0. The algorithm for determining delta is simply maximum - minimum.
+This function checks if the tractor is in a stationary state. A preset number of latest records(NUM_OF_SS_RECORDS) are read out of the accelerometer buffer. If each accelerometer axis shows a delta smaller than SS_DELTATHRESHOLD , then the tractor is assumed to be stationary, and the function returns 1. If any accelerometer delta exceeds SS_DELTATHRESHOLD, the function returns 0. The algorithm for determining delta is simply maximum - minimum.
 */
 int checkIfSS(int rcv_buf_pos){
-	int i = ss_numrecords;
+	int i = NUM_OF_SS_RECORDS;
 	double x_min = DBL_MAX, x_max = -1 * DBL_MAX;
 	double y_min = DBL_MAX, y_max = -1 * DBL_MAX;
 	double z_min = DBL_MAX, z_max = -1 * DBL_MAX;
 
 	//printf("Buf Pos = %d\n", rcv_buf_pos);
 	do{
-		if(rcv_buf_pos == begin_rcv_buf){
+		if(rcv_buf_pos == BEGIN_RCV_BUF){
 			//wrap around to correct position
 		}
 		if(x_acc_rcv[rcv_buf_pos] > x_max){x_max = x_acc_rcv[rcv_buf_pos];}
@@ -142,9 +142,9 @@ int checkIfSS(int rcv_buf_pos){
 	//test
 	printf("x_delta = %lf, y_delta = %lf, z_delta = %lf \n", x_max - x_min, y_max - y_min, z_max - z_min);
 
-	if( ((x_max - x_min) < ss_deltathreshold) && 
-		((y_max - y_min) < ss_deltathreshold) && 
-		((z_max - z_min) < ss_deltathreshold)){
+	if( ((x_max - x_min) < SS_DELTATHRESHOLD) && 
+		((y_max - y_min) < SS_DELTATHRESHOLD) && 
+		((z_max - z_min) < SS_DELTATHRESHOLD)){
 		printf("System is at rest\n");
 		return 1;
 	}
@@ -162,12 +162,12 @@ The gyro data sent is always relative to the latest pitch/roll data, again to pr
 Called when the system is determined to be at rest. 
 */
 abs_pos getAbsPos(int rcv_buf_pos){
-	int i = ss_numrecords;
+	int i = NUM_OF_SS_RECORDS;
 	double x_acc_rcvavg = 0, y_acc_rcvavg = 0;
 	abs_pos radians;
 
 	do{
-		if(rcv_buf_pos == begin_rcv_buf){
+		if(rcv_buf_pos == BEGIN_RCV_BUF){
 			//wrap around to correct position
 		}
 		x_acc_rcvavg+= x_acc_rcv[rcv_buf_pos];
@@ -175,8 +175,8 @@ abs_pos getAbsPos(int rcv_buf_pos){
 		rcv_buf_pos--;
 	}while(i--);
 
-	x_acc_rcvavg = (x_acc_rcvavg/ss_numrecords);
-	y_acc_rcvavg = (y_acc_rcvavg/ss_numrecords);
+	x_acc_rcvavg = (x_acc_rcvavg/NUM_OF_SS_RECORDS);
+	y_acc_rcvavg = (y_acc_rcvavg/NUM_OF_SS_RECORDS);
 	radians.pitch = asin (y_acc_rcvavg);
 	radians.roll  = asin (x_acc_rcvavg);
 	//Test
@@ -191,10 +191,10 @@ This function calculates the bias of the gyros. Called when the system is determ
 */
 
 gyro_bias getGyroBias(int rcv_buf_pos){
-	int i = ss_numrecords;
+	int i = NUM_OF_SS_RECORDS;
 	gyro_bias bias;
 	do{
-		if(rcv_buf_pos == begin_rcv_buf){
+		if(rcv_buf_pos == BEGIN_RCV_BUF){
 			//wrap around to correct position
 		}
 		bias.xAxis+= x_head_rcv[rcv_buf_pos];
@@ -203,9 +203,9 @@ gyro_bias getGyroBias(int rcv_buf_pos){
 		rcv_buf_pos--;
 	}while(i--);
 
-	bias.xAxis = bias.xAxis/ss_numrecords;
-	bias.yAxis = bias.yAxis/ss_numrecords;
-	bias.zAxis = bias.zAxis/ss_numrecords;
+	bias.xAxis = bias.xAxis/NUM_OF_SS_RECORDS;
+	bias.yAxis = bias.yAxis/NUM_OF_SS_RECORDS;
+	bias.zAxis = bias.zAxis/NUM_OF_SS_RECORDS;
 	//Test
 	printf("X Gyro bias: %lf, Y Gyro bias: %lf, Z Gyro bias: %lf\n", bias.xAxis, bias.yAxis, bias.zAxis);
 	return bias;
@@ -230,4 +230,14 @@ This function takes the raw acceleration data, multiplies it by a constant and s
 
 void sendAccData(){
 
+}
+
+/*
+ recvData()
+ Temporary function for written for compilation purposes. Function to be used is written by Jesper. 
+ */
+ 
+packet_load recvData(){
+    
+    
 }
