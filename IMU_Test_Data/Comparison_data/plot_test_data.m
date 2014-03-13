382b7349bab65c7bda93ca7d75688c5f27c8c905
Jesper Hamfelt <jesper.hamfelt@gmail.com>
2014-03-13 10:08:39 +0100

    Jesper, cleaning logfiltes

---
+++ IMU_Test_Data/Comparison_data/plot_test_data.m
@@ -0,0 +1,24 @@
+figure
+Data = load('IMUTestData.mat');
+
+
+plot(1:length(Data.SIM_X),Data.SIM_X,'b');
+hold on
+plot(1:length(Data.WL_X),Data.WL_X,'r');
+title('X-axis rotations');
+legend('IMU1', 'IMU2');
+xlabel('Sample number');
+ylabel('Rotation (radians)');
+axis tight
+hold off
+
+figure
+plot(1:length(Data.SIM_Y),Data.SIM_Y,'b');
+hold on
+plot(1:length(Data.WL_Y),Data.WL_Y,'r');
+title('Y-axis rotations');
+legend('IMU1', 'IMU2');
+xlabel('Sample number');
+ylabel('Rotation (radians)');
+axis tight
+hold off
